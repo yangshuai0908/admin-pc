6b8a5dabc65e84e623feaf1717283ee34273c81a
WINDOWS-7FLNGHS\Administrator <xinkeji368@163.com>
2025-12-22 17:46:33 +0800

    ``` feat(element-plus): 设置Element Plus为中文语言
    将Element Plus的默认语言设置为中文，提升中文用户的使用体验。
    ```

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -7,13 +7,15 @@ import 'element-plus/dist/index.css'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import 'animate.css'
 import './styles/index.scss'
-
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
 const app = createApp(App)
 
 const pinia = createPinia()
 app.use(pinia)
 app.use(router)
-app.use(ElementPlus)
+app.use(ElementPlus, {
+  locale: zhCn, // 设置中文
+})
 
 // 全局注册 Element Plus 图标
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
